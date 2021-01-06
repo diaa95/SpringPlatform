2ab91023428d72f0a65a40ea38c9469274d6181d
Diaa AbdAlDayem <Diaa.abdaldayem@gmail.com>
2021-01-06 23:03:03 +0200

    Show Assignment

--- mvc/src/main/java/com/diaa/mvc/controllers/BooksController.java
+++ mvc/src/main/java/com/diaa/mvc/controllers/BooksController.java
@@ -5,20 +5,19 @@ import com.diaa.mvc.services.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 public class BooksController {
-    private final BookService bookService;
 
+    private final BookService bookService;
     public BooksController(BookService bookService) {
         this.bookService = bookService;
     }
+
 
     @RequestMapping("/books")
     public String index(Model model) {
@@ -26,10 +25,22 @@ public class BooksController {
         model.addAttribute("books", books);
         return "books/index.jsp";
     }
+
+
+    @RequestMapping("/books/{id}")
+    public String show(Model model, @PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        model.addAttribute("book", book);
+        return "books/show.jsp";
+    }
+
+    
     @RequestMapping("/books/new")
     public String newBook(@ModelAttribute("book") Book book){
         return "books/add.jsp";
     }
+
+
     @RequestMapping(value = "/books", method = RequestMethod.POST)
     public String creat(@Valid @ModelAttribute("book") Book book, BindingResult result){
         if (result.hasErrors()){
